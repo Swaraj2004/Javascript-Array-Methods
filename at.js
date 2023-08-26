0f0b5e71cb32e96efb600f2aa32a2c9853969628
Swaraj <swarajchavan2004@gmail.com>
2023-08-26 23:19:31 +0530

    added at

---
+++ at.js
@@ -0,0 +1,15 @@
+// The at() method returns an indexed element from an array.
+// The at() method returns the same as [].
+// Syntax: array.at(index)
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits.at(2);
+console.log(fruit);
+
+// Get the first element of fruits
+fruit = fruits.at();
+console.log(fruit);
+
+// Get the last element of fruits
+fruit = fruits.at(-1);
+console.log(fruit);
