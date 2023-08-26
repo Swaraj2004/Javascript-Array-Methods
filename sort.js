89e2571c9e040a8026d256d49fa1ef4fb77c50e6
Swaraj <swarajchavan2004@gmail.com>
2023-08-26 23:05:35 +0530

    added sort

---
+++ sort.js
@@ -0,0 +1,19 @@
+// The sort() sorts the elements of an array.
+// The sort() overwrites the original array.
+// The sort() sorts the elements as strings in alphabetical and ascending order.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+console.log(fruits);
+
+// Sorting alphabetically works well for strings ("Apple" comes before "Banana").
+// But, sorting numbers can produce incorrect results.
+// "25" is bigger than "100", because "2" is bigger than "1".
+
+const points = [40, 100, 1, 5, 25, 10];
+// Sort in ascending order
+points.sort((a, b) => a - b);
+console.log(points);
+// Sort in descending order
+points.sort((a, b) => b - a);
+console.log(points);
