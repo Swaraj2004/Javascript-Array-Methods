9d154b923292f0e7a2623d0c661607a1f30590e2
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 00:05:48 +0530

    added includes

---
+++ includes.js
@@ -0,0 +1,9 @@
+// The includes() method returns true if an array contains a specified value.
+// The includes() method returns false if the value is not found.
+// The includes() method is case sensitive.
+// Syntax: array.includes(element, start)
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.includes("Mango"));
+// Start the search at position 3
+console.log(fruits.includes("Banana", 3));
