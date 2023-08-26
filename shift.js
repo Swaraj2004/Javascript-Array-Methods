4b52f2b801e61497e7d2532e6511063dc6684f77
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 00:28:51 +0530

    added shift

---
+++ shift.js
@@ -0,0 +1,7 @@
+// The shift() method removes the first item of an array.
+// The shift() method changes the original array.
+// The shift() method returns the shifted element.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.shift());
+console.log(fruits);
