f10d03cf4f181bdbe27b4c63f91147c350358f9b
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 00:23:06 +0530

    added push

---
+++ push.js
@@ -0,0 +1,9 @@
+// The push() method adds new items to the end of an array.
+// The push() method changes the length of the array.
+// The push() method returns the new length.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.push("Kiwi"));
+console.log(fruits);
+fruits.push("Lemon", "Lichi");
+console.log(fruits);
