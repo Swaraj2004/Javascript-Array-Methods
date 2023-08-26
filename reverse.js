10265f2417d585b9c978017e43bc5301025fe23c
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 00:15:15 +0530

    added reverse

---
+++ reverse.js
@@ -0,0 +1,10 @@
+// The reverse() method reverses the order of the elements in an array.
+// The reverse() method overwrites the original array.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.reverse());
+
+// Reverse a string
+const str = "Coding is fun!";
+const result = str.split("").reverse().join("");
+console.log(result);
