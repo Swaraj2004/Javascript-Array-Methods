620986fbe71fb6d2cb32e674f27af7c8baf36b8d
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 00:09:05 +0530

    added join

---
+++ join.js
@@ -0,0 +1,7 @@
+// The join() method returns an array as a string.
+// The join() method does not change the original array.
+// Any separator can be specified. The default is comma (,).
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.join());
+console.log(fruits.join(" and "));
