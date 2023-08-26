dbc2b31be77c112b164df2273f57598b40574f0e
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 00:26:53 +0530

    added unshift

---
+++ unshift.js
@@ -0,0 +1,7 @@
+// The unshift() method adds new elements to the beginning of an array.
+// The unshift() method overwrites the original array.
+// The unshift() method returns the new length.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.unshift("Lemon", "Pineapple"));
+console.log(fruits);
