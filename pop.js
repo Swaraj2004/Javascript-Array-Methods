83e76477e2f841d8a1938ae98f9e252b01107745
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 00:23:52 +0530

    added pop

---
+++ pop.js
@@ -0,0 +1,7 @@
+// The pop() method removes (pops) the last element of an array.
+// The pop() method changes the original array.
+// The pop() method returns the removed element.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.pop());
+console.log(fruits);
