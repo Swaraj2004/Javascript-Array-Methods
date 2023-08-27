938e71bbd3aa3c1730c76877d48e5b698eb42259
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 09:39:30 +0530

    added isArray

---
+++ isarray.js
@@ -0,0 +1,11 @@
+// The isArray() method returns true if an object is an array, otherwise false.
+// Array.isArray() is a static property of the JavaScript Array object.
+// You can only use it as Array.isArray().
+// Using x.isArray(), where x is an array will return undefined.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let result = Array.isArray(fruits);
+console.log(result);
+let text = "W3Schools";
+result = Array.isArray(text);
+console.log(result);
