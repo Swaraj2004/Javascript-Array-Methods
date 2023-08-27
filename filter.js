8ba9677e5a0c85c3bb4971e904638e4da32ed4f8
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 13:58:50 +0530

    updated filter

--- filter.js
+++ filter.js
@@ -6,11 +6,11 @@
 //Return an array of all values in ages[] that are 18 or over
 const ages = [32, 33, 16, 40];
 const result = ages.filter((age) => age >= 18);
-console.log(result);
+console.log(result); // [ 32, 33, 40 ]
 
 // Filter unique values from array
 const numbers = [1, 2, 3, 2, 1, 4, 5, 4, 6, 5];
 const output = numbers.filter(
   (value, index, arr) => arr.indexOf(value) === index
 );
-console.log(output);
+console.log(output); // [ 1, 2, 3, 4, 5, 6 ]
