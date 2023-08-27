07de565a76ddf65493843cedb4b583b5d6c261fc
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 11:58:57 +0530

    added flat

---
+++ flat.js
@@ -0,0 +1,15 @@
+// The flat() method concatenates sub-array elements.
+// The flat() method creates a new array
+// Syntax: array.flat(depth)
+
+const myArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+const newArr = myArr.flat();
+console.log(newArr);
+
+const myArr2 = [1, 2, [3, [4, 5, 6], 7], 8];
+const newArr2 = myArr2.flat(2);
+console.log(newArr2);
