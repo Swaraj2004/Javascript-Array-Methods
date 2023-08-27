fc1033c2aa0f0de4b2e3a18b42060ae13c30d5a1
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 11:35:44 +0530

    added findIndex

---
+++ findindex.js
@@ -0,0 +1,10 @@
+// The findIndex() method executes a function for each array element.
+// The findIndex() method returns the index (position) of the first element that passes a test.
+// The findIndex() method returns -1 if no match is found.
+// The findIndex() method does not execute the function for empty array elements.
+// The findIndex() method does not change the original array.
+// Syntax: array.findIndex(function(currentValue, index, arr), thisValue)
+
+const ages = [3, 10, 18, 20];
+const output = ages.findIndex((age) => age > 18);
+console.log(output);
