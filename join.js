21d389292304b672facbe626d17d60c702035bdd
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 14:09:38 +0530

    updated join

--- join.js
+++ join.js
@@ -3,5 +3,5 @@
 // Any separator can be specified. The default is comma (,).
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
-console.log(fruits.join());
-console.log(fruits.join(" and "));
+console.log(fruits.join()); // Banana,Orange,Apple,Mango
+console.log(fruits.join(" and ")); // Banana and Orange and Apple and Mango
