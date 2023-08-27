795b923a47a782137837f3080ddb604454564b9e
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 10:44:23 +0530

    added valueOf

---
+++ valueof.js
@@ -0,0 +1,11 @@
+// The valueOf() method returns the array itself.
+// The valueOf() method does not change the original array.
+// fruits.valueOf() returns the same as fruits.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const myArray = fruits.valueOf();
+console.log(myArray);
+
+// fruits.valueOf() returns the same as fruits
+const myArr = fruits;
+console.log(myArr);
