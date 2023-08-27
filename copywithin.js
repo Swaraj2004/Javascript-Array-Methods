c662c13209faf42b5254a1bee1c454ea7d270d7c
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 09:47:21 +0530

    added copyWithin

---
+++ copywithin.js
@@ -0,0 +1,12 @@
+// The copyWithin() method copies array elements to another position in the array.
+// The copyWithin() method overwrites the existing values.
+// The copyWithin() method does not add items to the array.
+// Syntax: array.copyWithin(target, start, end)
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.copyWithin(2, 0);
+console.log(fruits);
+
+const newFruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+newFruits.copyWithin(2, 0, 2);
+console.log(newFruits);
