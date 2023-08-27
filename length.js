4cb366ebaafe1e4e1a94fe5750988a5bcd7d5107
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 10:41:39 +0530

    added length

---
+++ length.js
@@ -0,0 +1,8 @@
+// The length property sets or returns the number of elements in an array.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let length = fruits.length;
+console.log(length);
+
+fruits.length = 2;
+console.log(fruits);
