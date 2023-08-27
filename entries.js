98e3a6ead9213e3ea738238cd7c167cff6495007
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 14:05:10 +0530

    updated entries

--- entries.js
+++ entries.js
@@ -1,8 +1,6 @@
 // The entries() method returns an Array Iterator object with key/value pairs:
 // [0, "Banana"]
 // [1, "Orange"]
-// [2, "Apple"]
-// [3, "Mango"]
 // The entries() method does not change the original array.
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
@@ -11,7 +9,17 @@ const iterator = fruits.entries();
 for (let x of iterator) {
   console.log(x);
 }
+// Output:
+// [ 0, 'Banana' ]
+// [ 1, 'Orange' ]
+// [ 2, 'Apple' ]
+// [ 3, 'Mango' ]
 
 for (const [i, el] of fruits.entries()) {
   console.log(i, el);
 }
+// Output:
+// 0 Banana
+// 1 Orange
+// 2 Apple
+// 3 Mango
