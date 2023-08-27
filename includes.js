3853de5ff973a408afda47cb8a958f33c3c7dfce
Swaraj <swarajchavan2004@gmail.com>
2023-08-27 14:07:26 +0530

    updated includes

--- includes.js
+++ includes.js
@@ -4,6 +4,6 @@
 // Syntax: array.includes(element, start)
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
-console.log(fruits.includes("Mango"));
+console.log(fruits.includes("Mango")); // true
 // Start the search at position 3
-console.log(fruits.includes("Banana", 3));
+console.log(fruits.includes("Banana", 3)); // false
